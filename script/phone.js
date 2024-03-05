8045b479d3f29a457ead8f08a925f5897f814a43
ANIK-MOHANTA24 <mohontoa050@gmail.com>
2024-03-05 19:52:19 +0600

    first commit

---
+++ script/phone.js
@@ -0,0 +1,41 @@
+// console.log('phone hunting');
+const loadPhone = async () => {
+  const res = await fetch(
+    'https://openapi.programming-hero.com/api/phones?search=iphone'
+  );
+  const data = await res.json();
+  const phones = data.data;
+  // console.log(phones);
+  displayPhone(phones);
+};
+
+const displayPhone = phones => {
+  // console.log(phones);
+
+  // step-1: GET ID
+  const phoneContainer = document.getElementById('phone_container');
+  phones.forEach(phone => {
+    console.log(phone);
+
+    // step-2: create a div
+    const phoneCard = document.createElement('div');
+    phoneCard.classList = `card bg-gray-100 shadow-xl p-6`;
+
+    // step-3: set innerHTML
+    phoneCard.innerHTML = `
+      <figure><img src="${phone.image}"  alt="Shoes" /> 
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">${phone.phone_name}</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-center">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+      `;
+    // step-4: append child
+    phoneContainer.appendChild(phoneCard);
+  });
+};
+
+loadPhone();
